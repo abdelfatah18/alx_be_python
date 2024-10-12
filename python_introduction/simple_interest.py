23a475ac5ecf33a8e87a7f09ae218c096dedb91c
abdelfatah hany <aaa5122003@gmail.com>
2024-10-12 07:34:39 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,6 +2,6 @@ principal = 1000
 rate = 0.05
 time = 3
 
-sum = principal* rate *time
+interest = principal* rate *time
 
-print(f"The simple interest is:{sum}")
+print(f"The simple interest is:{interest}")
