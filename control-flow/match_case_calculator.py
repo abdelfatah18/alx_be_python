5803e88e31ac3c09bf92e2cc2b268ea2e3fbe7a9
abdelfatah hany <aaa5122003@gmail.com>
2024-10-17 20:12:04 +0300

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,22 +1,22 @@
 num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-operation = input("Choose the operation (+, -, *, /): ")
+    num2 = int(input("Enter the second number: "))
+    operation = input("Choose the operation (+, -, *, /): ")
 
-if operation == "+":
-    result = num1 + num2
-    print(f"The result is {result}.")
-elif operation == "-":
-    result = num1 - num2
-    print(f"The result is {result}.")
-elif operation == "*":
-    result = num1 * num2
-    print(f"The result is {result}.")
-elif operation == "/":
-    if num2 == 0:
-        print("Cannot divide by zero.")
-    else:
-        result = num1 / num2
-        print(f"The result is {result}.")
+    if operation == '+':
+        result = num1 + num2
+        print(f"The result of {num1} + {num2} is {result}.")
+    elif operation == '-':
+        result = num1 - num2
+        print(f"The result of {num1} - {num2} is {result}.")
+    elif operation == '*':
+        result = num1 * num2
+        print(f"The result of {num1} * {num2} is {result}.")
+    elif operation == '/':
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print(f"The result of {num1} / {num2} is {result:.2f}.")  
 
 
  
