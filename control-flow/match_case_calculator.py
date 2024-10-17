706cd14061ab702982c77a28480b4213885ca8f7
abdelfatah hany <aaa5122003@gmail.com>
2024-10-17 20:08:49 +0300

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,27 +1,23 @@
-num1=int(input("Enter the first number: "))
-num2=int(input("Enter the second number: "))
-operation=input("Choose the operation (+, -, *, /):")
- 
-if operation=="+" :
-    result=num1+num2
-    print(f"The result is {result}.")
-    
- 
-elif operation=="-" :
-    result=num1-num2
-    print(f"The result is {result}.")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
 
- 
-elif operation=="*" :
-    result=num1*num2
+if operation == "+":
+    result = num1 + num2
     print(f"The result is {result}.")
- 
-elif operation=="/" :
-    if num2==0 :
-     print("Cannot divide by zero.")
-    else :
-        result=num1/num2
+elif operation == "-":
+    result = num1 - num2
+    print(f"The result is {result}.")
+elif operation == "*":
+    result = num1 * num2
+    print(f"The result is {result}.")
+elif operation == "/":
+    if num2 == 0:
+        print("Cannot divide by zero.")
+    else:
+        result = num1 / num2
         print(f"The result is {result}.")
- 
+else:
+    print("Invalid operation. Please choose +, -, *, or /.")
 
  
