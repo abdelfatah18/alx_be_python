37297bafb8af02a5a6764d4264789d0cc6cee81c
abdelfatah hany <aaa5122003@gmail.com>
2024-10-18 17:36:02 +0300

    Create pattern_drawing.py

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+number = int(input("Enter the size of the pattern: "))
+
+row = 0
+
+while row < number:
+    
+    for col in range(number):
+        print("*", end="")
+
+    print()
+
+    row += 1
