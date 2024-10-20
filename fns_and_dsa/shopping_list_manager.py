609b6adb30ec2b8772cbd11ffb70a1d2bf1fc35e
abdelfatah hany <aaa5122003@gmail.com>
2024-10-20 23:57:52 +0300

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,3 +1,4 @@
+
 def display_menu():
     print("Shopping List Manager")
     print("1. Add Item")
@@ -8,42 +9,26 @@ def display_menu():
     
 def main():
     shopping_list = []
-    
     while True:
         display_menu()
-        
-        # Input validation for menu choice
-        try:
-            choice =input("Enter your choice: ")
-        except ValueError:
-            print("Please enter a valid number.")
-            continue
-            
-        if choice == 1:
-            item = input("Enter your item: ")
+        choice = int(input("Enter your choice: "))
+        if choice == 1 :
+            item=input("enter your item : "))
+            print(f"item {item}  is added")
             shopping_list.append(item)
-            print(f"Item '{item}' has been added to the shopping list.")
-        
         elif choice == 2:
-            item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
-                print(f"Item '{item}' has been removed from the shopping list.")
+                print(f"{item} has been removed from the shopping list.")
             else:
-                print(f"Item '{item}' not found in the shopping list.")
+                print(f"{item} not found in the shopping list.")
             
         elif choice == 3:
-            if shopping_list:
-                print("The shopping list contains:")
-                for item in shopping_list:
-                    print(f"- {item}")
-            else:
                 print("The shopping list is currently empty.")
             
         elif choice == 4:
             print("Goodbye!")
             break
-            
         else:
             print("Invalid choice. Please try again.")
             
