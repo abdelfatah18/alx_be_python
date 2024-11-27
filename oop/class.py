8e532fa97248e268cd6d656db41ba980d942f56e
abdelfatah hany <aaa5122003@gmail.com>
2024-11-27 17:46:35 +0200

    Create class.py

---
+++ oop/class.py
@@ -0,0 +1,26 @@
+class Book:
+    def __init__(self, title, author, year):
+        """
+        Constructor that initializes a Book instance with a title, author, and year.
+        """
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """
+        Destructor that prints a message when a Book instance is deleted.
+        """
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """
+        String representation of the Book instance.
+        """
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """
+        Official representation of the Book instance.
+        """
+        return f"Book('{self.title}', '{self.author}', {self.year})"
